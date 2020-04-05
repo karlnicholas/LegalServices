8a20ca13750c1f0bd23b3c40ad3d66e646dd4af0
Karl Nicholas <karl.nicholas@outlook.com>
2020-04-04 21:05:14 -0700

    Updates

--- GuidedSearchAngular/src/app/statute.service.ts
+++ GuidedSearchAngular/src/app/statute.service.ts
@@ -1,16 +1,42 @@
 import { Injectable } from '@angular/core';
 import { Statute } from './Statute';
-import { STATUTES } from './mock-statutes';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StatuteService {
 
-  constructor() { }
+constructor(
+  private http: HttpClient) { }
+  private statutesUrl = 'api/statutes';  // URL to web api
 
-  getStatutes(): Observable<Statute[]> {
-    return of(STATUTES);
-  }
+	/** GET heroes from the server */
+	getStatutes (): Observable<Statute[]> {
+	  return this.http.get<Statute[]>(this.statutesUrl)
+	    .pipe(
+//	      tap(_ => this.log('fetched statutes')),
+	      catchError(this.handleError<Statute[]>('getStatutes', []))
+	    );
+	}	/**
+	 * Handle Http operation that failed.
+	 * Let the app continue.
+	 * @param operation - name of the operation that failed
+	 * @param result - optional value to return as the observable result
+	 */
+	private handleError<T> (operation = 'operation', result?: T) {
+	  return (error: any): Observable<T> => {
+	
+	    // TODO: send the error to remote logging infrastructure
+	    console.error(error); // log to console instead
+	
+	    // TODO: better job of transforming error for user consumption
+//	    this.log(`${operation} failed: ${error.message}`);
+	
+	    // Let the app keep running by returning an empty result.
+	    return of(result as T);
+	  };
+	}
 }
