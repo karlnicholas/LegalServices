e62a30322c066a5dac80bd963f121f4eb2444767
Karl Nicholas <Karl.Nicholas@outlook.com>
2020-08-02 18:47:00 -0700

    updates jquery

--- GuidedSearchNpm/src/scripts/index.js
+++ GuidedSearchNpm/src/scripts/index.js
@@ -1,3 +1,29 @@
 import '../styles/index.scss';
-
 console.log('webpack starterkit');
+function getURLParameters() {
+	var sPageURL = window.location.search.substring(1);
+	return sPageURL.split('&');
+}
+$( document ).ready(function() {
+	var sURLVariables = getURLParameters();
+	var path = "";
+	for (var i = 0; i < sURLVariables.length; i++) {
+		var sParameter = sURLVariables[i].split('=');
+		if (sParameter[0].toLowerCase() === 'path' ) {
+			path = sParameter[1];
+		}
+		console.log(sParameter[0] + ":" + sParameter[1]);
+	}
+	$.getJSON( "http://localhost:8080?path="+path, function( viewModel ) {
+	  var items = [];
+	  $.each( viewModel.entries, function( key, value ) {
+		items.push( "<div class='row'>" );
+		items.push( "<div class='col-sm-4'>" + value.displayTitle + "</div>" );
+	    items.push( "<div class='col-sm-6'>" + value.statutesBaseClass.title + "</div>" );
+	    items.push( "<div class='col-sm-2'>§§ " + value.statutesBaseClass.statuteRange.sNumber.sectionNumber + " - " + value.statutesBaseClass.statuteRange.eNumber.sectionNumber + "</div>" );
+		items.push( "</div>" );
+	  });
+	  $('#cand').html(items.join( "" ));
+	});
+});
+
